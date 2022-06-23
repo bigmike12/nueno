e96521c718f9620d7a71c494cb72bee9f3f0de5f
bigmike12 <dj.buckwild12@gmail.com>
2022-06-23 11:43:51 +0100

    Included Drag and Drop, ability to add and remove fields

--- pages/jobs/new/[id]/application-form.tsx
+++ pages/jobs/new/[id]/application-form.tsx
@@ -2,20 +2,27 @@ import { ApplicationFormsCreateRequestParams } from "@api-contracts/application-
 import { DotsVerticalIcon, TrashIcon } from "@heroicons/react/outline";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { BaseSyntheticEvent, useState } from "react";
+import React, { BaseSyntheticEvent, useState } from "react";
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import { v4 as uuidv4 } from "uuid";
 
 import { asStringOrUndefined } from "@helpers/type-safety";
 
 import Shell from "@components/Shell";
 
+interface Fields {
+  id: string;
+  input: string;
+}
+
 export default function JobsNew() {
   const router = useRouter();
   const jobUid = asStringOrUndefined(router.query.id);
 
-  const [fields, setFields] = useState([{ id: Math.floor(Math.random() * 1000), input: "" }]);
+  const [fields, setFields] = useState<Array<Fields>>([]);
 
   const handleSetFields = () => {
-    setFields([...fields, { id: Math.floor(Math.random() * 1000), input: "" }]);
+    setFields([...fields, { id: uuidv4(), input: "short_text" }]);
   };
 
   const handleDeleteField = (index: number) => {
@@ -37,103 +44,169 @@ export default function JobsNew() {
       console.log(e);
     }
   }
-  return (
-    <Shell>
-      <header>
-        <div className="py-6 mx-auto max-w-7xl sm:px-6 md:px-0 lg:flex lg:items-center lg:justify-between">
-          <h1 className="text-3xl font-bold text-gray-900">Application form</h1>
-          <span className="sm:ml-3">
-            <button
-              type="button"
-              onClick={submit}
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Publish
-            </button>
-          </span>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="md:grid md:grid-cols-3 md:gap-6">
-            <div className="mt-5 md:mt-0 md:col-span-2">
-              <div className="shadow sm:rounded-md sm:overflow-hidden">
-                <div className="px-4 py-5 space-y-2 bg-white sm:p-6">
-                  <h2 className="text-lg font-semibold text-gray-700">Customize your application form</h2>
-                  <div className="flex items-center">
-                    <DotsVerticalIcon className="w-5 h-5 text-gray-400 cursor-move" aria-hidden="true" />
-                    <div className="flex-auto p-2 bg-gray-100 rounded">
-                      <p className="text-md">First name</p>
-                    </div>
-                  </div>
-                  <div className="flex items-center">
-                    <DotsVerticalIcon className="w-5 h-5 text-gray-400 cursor-move" aria-hidden="true" />
-                    <div className="flex-auto p-2 bg-gray-100 rounded">
-                      <p className="text-md">Last name</p>
-                    </div>
-                  </div>
-                  <div className="flex items-center">
-                    <DotsVerticalIcon className="w-5 h-5 text-gray-400 cursor-move" aria-hidden="true" />
-                    <div className="flex-auto p-2 bg-gray-100 rounded">
-                      <p className="text-md">Email</p>
-                    </div>
-                  </div>
 
-                  {fields.map((field, index) => (
-                    <div className="flex items-center" key={index}>
-                      <DotsVerticalIcon className="w-5 h-5 text-gray-400 cursor-move" aria-hidden="true" />
-                      <div className="flex-auto p-2 bg-gray-100 rounded">
-                        <div className="flex justify-between">
-                          <p className="text-sm font-semibold text-md">Type</p>
-                          <TrashIcon
-                            className="w-4 h-4 text-red-500 cursor-pointer"
-                            aria-hidden="true"
-                            onClick={() => handleDeleteField(index)}
-                          />
+  const reorder = (list: Fields[], startIndex: number, endIndex: number | undefined) => {
+    const result: any = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const onDragEnd = (result: DropResult): void => {
+    console.log("end", result);
+    result.destination?.droppableId;
+
+    if (
+      result.destination?.droppableId === result.source?.droppableId &&
+      result.destination?.index === result.source?.index
+    ) {
+      return;
+    }
+
+    const items: Fields[] = reorder(fields, result.source?.index, result.destination?.index);
+    setFields(items);
+  };
+
+  const handleType = (e: React.ChangeEvent<{ value: string }>, id: string) => {
+    const stringifiedFields = JSON.stringify(fields);
+    const parsedFields = JSON.parse(stringifiedFields);
+    const field = parsedFields && parsedFields.filter((field: Fields) => field.id === id);
+    field[0].input = e.target.value;
+    setFields(parsedFields);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="form">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            <Shell>
+              <header>
+                <div className="py-6 mx-auto max-w-7xl sm:px-6 md:px-0 lg:flex lg:items-center lg:justify-between">
+                  <h1 className="text-3xl font-bold text-gray-900">Application form</h1>
+                  <span className="sm:ml-3">
+                    <button
+                      type="button"
+                      onClick={submit}
+                      className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Publish
+                    </button>
+                  </span>
+                </div>
+              </header>
+              <main>
+                <div>
+                  <div className="md:grid md:grid-cols-3 md:gap-6">
+                    <div className="mt-5 md:mt-0 md:col-span-2">
+                      <div className="shadow sm:rounded-md sm:overflow-hidden">
+                        <div className="px-4 py-5 space-y-2 bg-white sm:p-6">
+                          <h2 className="text-lg font-semibold text-gray-700">
+                            Customize your application form
+                          </h2>
+                          <div className="flex items-center">
+                            <DotsVerticalIcon
+                              className="w-5 h-5 text-gray-400 cursor-move"
+                              aria-hidden="true"
+                            />
+                            <div className="flex-auto p-2 bg-gray-100 rounded">
+                              <p className="text-md">First name</p>
+                            </div>
+                          </div>
+                          <div className="flex items-center">
+                            <DotsVerticalIcon
+                              className="w-5 h-5 text-gray-400 cursor-move"
+                              aria-hidden="true"
+                            />
+                            <div className="flex-auto p-2 bg-gray-100 rounded">
+                              <p className="text-md">Last name</p>
+                            </div>
+                          </div>
+                          <div className="flex items-center">
+                            <DotsVerticalIcon
+                              className="w-5 h-5 text-gray-400 cursor-move"
+                              aria-hidden="true"
+                            />
+                            <div className="flex-auto p-2 bg-gray-100 rounded">
+                              <p className="text-md">Email</p>
+                            </div>
+                          </div>
+
+                          {fields.map(({ id }, index) => (
+                            <Draggable draggableId={id} index={index} key={id}>
+                              {(provided) => (
+                                <div
+                                  className="flex items-center"
+                                  key={index}
+                                  {...provided.dragHandleProps}
+                                  {...provided.draggableProps}
+                                  ref={provided.innerRef}>
+                                  <DotsVerticalIcon
+                                    className="w-5 h-5 text-gray-400 cursor-move"
+                                    aria-hidden="true"
+                                  />
+                                  <div className="flex-auto p-2 bg-gray-100 rounded">
+                                    <div className="flex justify-between">
+                                      <p className="text-sm font-semibold text-md">Type</p>
+                                      <TrashIcon
+                                        className="w-4 h-4 text-red-500 cursor-pointer"
+                                        aria-hidden="true"
+                                        onClick={() => handleDeleteField(index)}
+                                      />
+                                    </div>
+                                    <select
+                                      defaultValue={"type"}
+                                      className="block w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                      onChange={(e) => handleType(e, id)}
+                                      value={fields[index].input}>
+                                      <option value="short_text">Short text</option>
+                                      <option value="long_text">Long text</option>
+                                      <option value="checkbox">Yes/No</option>
+                                      <option value="select">Select one</option>
+                                      <option value="multi_select">Select multiple</option>
+                                    </select>
+                                    <p className="text-sm font-semibold text-md">Type</p>
+                                    <input
+                                      id="label"
+                                      name="label"
+                                      type="text"
+                                      required
+                                      className="block w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                      placeholder="e. g. LinkedIn URL"
+                                    />
+                                  </div>
+                                </div>
+                              )}
+                            </Draggable>
+                          ))}
+                          {provided.placeholder}
+
+                          {fields.length < 4 && (
+                            <div>
+                              <button
+                                type="button"
+                                onClick={handleSetFields}
+                                className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Add field
+                              </button>
+                            </div>
+                          )}
                         </div>
-                        <select
-                          defaultValue="short_text"
-                          className="block w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                          <option value="short_text">Short text</option>
-                          <option value="long_text">Long text</option>
-                          <option value="checkbox">Yes/No</option>
-                          <option value="select">Select one</option>
-                          <option value="multi_select">Select multiple</option>
-                        </select>
-                        <p className="text-sm font-semibold text-md">Type</p>
-                        <input
-                          id="label"
-                          name="label"
-                          type="text"
-                          required
-                          className="block w-full p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                          placeholder="e. g. LinkedIn URL"
-                        />
                       </div>
                     </div>
-                  ))}
-
-                  {fields.length < 4 && (
-                    <div>
-                      <button
-                        type="button"
-                        onClick={handleSetFields}
-                        className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Add field
-                      </button>
+                    <div className="md:col-span-1">
+                      <div className="px-4 sm:px-0">
+                        <h3 className="text-lg font-medium leading-6 text-gray-900">Tips</h3>
+                        <div className="mt-1 text-sm text-gray-600">...</div>
+                      </div>
                     </div>
-                  )}
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="md:col-span-1">
-              <div className="px-4 sm:px-0">
-                <h3 className="text-lg font-medium leading-6 text-gray-900">Tips</h3>
-                <div className="mt-1 text-sm text-gray-600">...</div>
-              </div>
-            </div>
+              </main>
+            </Shell>
           </div>
-        </div>
-      </main>
-    </Shell>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
